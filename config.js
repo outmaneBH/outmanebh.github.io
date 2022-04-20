42c092602aa2eba2913c58f8420c2c098f3ec584
Outmane Bouhou extern <Bouhou.Outmane@hpecds.com>
2022-04-20 11:51:40 +0200

    4 commit

--- config.js
+++ config.js
@@ -38,5 +38,5 @@ function fn1() {
 
 // Registering Service Worker
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js');
+  navigator.serviceWorker.register('./sw.js');
 }
